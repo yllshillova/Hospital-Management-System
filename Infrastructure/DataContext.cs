471a2dadb3d316abcbda8d446784e932910dbdc2
Yll Shillova <yllshillova@gmail.com>
2024-03-30 14:21:59 +0100

    changes made

--- Infrastructure/DataContext.cs
+++ Infrastructure/DataContext.cs
@@ -9,13 +9,6 @@ namespace Infrastructure
         {
         }
         public DbSet<User> Users { get; set; }
-        public DbSet<Patient> Patients { get; set; }
-        public DbSet<Receptionist> Receptionists { get; set; }
-        public DbSet<Appointment> Appointments { get; set; }
-        public DbSet<Doctor> Doctors { get; set; }
-        public DbSet<Symptom> Symptoms { get; set; }
-        public DbSet<Prescription> Prescriptions { get; set; }
-        public DbSet<Diagnosis> Diagnoses { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
