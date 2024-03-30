62aa6fa97bb73b2a59dac6988fc9b1d76fa95426
Yll Shillova <yllshillova@gmail.com>
2024-03-30 14:15:53 +0100

    Database Connection configuration created

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,67 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Receptionist> Receptionists { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Symptom> Symptoms { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Diagnosis> Diagnoses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Prescription>()
+            //    .HasOne(p => p.Doctor)
+            //    .WithMany()
+            //    .HasForeignKey(p => p.DoctorId)
+            //    .OnDelete(DeleteBehavior.Restrict);
+
+            //modelBuilder.Entity<Prescription>()
+            //    .HasOne(p => p.Patient)
+            //    .WithMany()
+            //    .HasForeignKey(p => p.PatientId)
+            //    .OnDelete(DeleteBehavior.Cascade);
+
+            //modelBuilder.Entity<Appointment>()
+            //    .HasOne(a => a.Patient)
+            //    .WithMany()
+            //    .HasForeignKey(a => a.PatientId)
+            //    .OnDelete(DeleteBehavior.Cascade);
+            //modelBuilder.Entity<Appointment>()
+            //    .HasOne(a => a.Receptionist)
+            //    .WithMany()
+            //    .HasForeignKey(a => a.ReceptionistId)
+            //    .OnDelete(DeleteBehavior.Restrict);
+
+            //modelBuilder.Entity<Diagnosis>()
+            //    .HasOne(d => d.Doctor)
+            //    .WithMany()
+            //    .HasForeignKey(d => d.DoctorId)
+            //    .OnDelete(DeleteBehavior.Restrict);
+
+            //modelBuilder.Entity<Diagnosis>()
+            //     .HasOne(d => d.Patient)
+            //     .WithMany()
+            //     .HasForeignKey(d => d.PatientId)
+            //     .OnDelete(DeleteBehavior.Cascade);
+
+            //modelBuilder.Entity<Diagnosis>()
+            //    .HasOne(d => d.Symptom)
+            //    .WithMany()
+            //    .HasForeignKey(d => d.SymptomId)
+            //    .OnDelete(DeleteBehavior.Restrict);
+        }
+
+    }
+}
