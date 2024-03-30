e83850eea068daab8bbe14cc91bd7bb6f8244c05
Dijane Hoti <dhotii002@gmail.com>
2024-03-30 14:58:38 +0100

    Change Patient Model

--- Domain/Entities/Patient.cs
+++ Domain/Entities/Patient.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class Patient
+    public class Patient
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
