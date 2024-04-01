3f84db08ac003c2e8279c97ccf27af6212d65d57
vesarrahmani <vesarrahmani17@gmail.com>
2024-04-01 11:40:31 +0100

    fix error on Labtest model

--- Domain/Entities/LabTest.cs
+++ Domain/Entities/LabTest.cs
@@ -5,9 +5,9 @@ namespace Domain.Entities
     public class LabTest
     {
         public Guid Id { get; set; }
-        public String Type { get; set; }
-        public String Results { get; set; }
-        public String Notes { get; set; }
+        public string Type { get; set; }
+        public string Results { get; set; }
+        public string Notes { get; set; }
         public Guid LaboratorScientistId { get; set; }
         public LaboratoryScientist laboratoryScientist { get; set; }
     }
