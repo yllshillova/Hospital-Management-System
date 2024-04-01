caf7c4419d14b7e02b6e78f1088ee36ff8f2664e
Yll Shillova <yllshillova@gmail.com>
2024-04-01 16:16:16 +0200

    Added repository and the port for Doctor entity

---
+++ Infrastructure/Repositories/DoctorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using Infrastructure.Base;
+
+namespace Infrastructure.Repositories
+{
+    internal class DoctorRepository : EntityBaseRepository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
