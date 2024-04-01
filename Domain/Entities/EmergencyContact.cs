c18cf0c4f8deac0dda13ee6ed6f6645aab79b123
FazliThaqi <120459098+FazliThaqi@users.noreply.github.com>
2024-04-01 12:24:15 +0200

    create class EmergencyContact

---
+++ Domain/Entities/EmergencyContact.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Domain.Entities
+{
+    internal class EmergencyContact
+    {
+        public Guid ContactId { get; set; }
+        public string ContactName {  get; set; }
+        public string Relation {  get; set; }
+        public string PhoneNumber { get; set; }
+        public Guid PatientId { get; set; }
+        public Patient Patient { get; set; }
+        
+    }
+}
