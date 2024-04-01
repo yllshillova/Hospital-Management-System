9e9eea7f56eb5c0103ea22684df76d9a450c2fb5
Dijane Hoti <dhotii002@gmail.com>
2024-04-02 00:37:28 +0200

    Patient Rapository

---
+++ Infrastructure/Repositories/PatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using Infrastructure.Base;
+
+namespace Infrastructure.Repositories
+{
+    internal class PatientRepository : EntityBaseRepository<Patient>, IPatientRepository
+    {
+        public PatientRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
