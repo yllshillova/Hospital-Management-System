b8a6abb8462d3431b971fd54e311fcb0e84853bb
Rinnor1 <rinoris100@gmail.com>
2024-04-01 12:41:23 +0200

    Added the Prescription Model

---
+++ Domain/Entities/Prescription.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class Prescription
+    {
+        public string Medicine { get; set; }
+        public string Dosage { get; set; }
+        public string Frequency { get; set; }
+        public string Route { get; set; }
+        public string FoodRelation { get; set; }
+        public List<string> DoDont { get; set; }
+        public Patient Patient { get; set; }
+        public Guid PatientId { get; set; }
+        public Doctor Doctor { get; set; }
+        public Guid DoctorId { get; set; }
+
+    }
+}
