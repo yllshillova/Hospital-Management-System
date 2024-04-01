ff1881c682d2e6607e9867a4f2669862e3545581
vesarrahmani <vesarrahmani17@gmail.com>
2024-04-01 23:46:33 +0100

    Implement Department Repository class & interface

---
+++ Infrastructure/Repositories/DepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using Infrastructure.Base;
+
+namespace Infrastructure.Repositories
+{
+    internal sealed class DepartmentRepository : EntityBaseRepository<Department>, IDepartmentRepository
+    {
+        public DepartmentRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
