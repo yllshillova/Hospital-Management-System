2f3e5fe0367c755b6b8b577d7a86562731772820
Yll Shillova <110238215+yllshillova@users.noreply.github.com>
2024-04-01 12:05:33 +0200

    Revert "Dev"

--- REST/Program.cs
+++ REST/Program.cs
@@ -1,7 +1,3 @@
-using Infrastructure;
-using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,12 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
 
 var app = builder.Build();
 
