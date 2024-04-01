7f88473a6c80476e8d2d1e6de8904cff6e100bfb
Dijane Hoti <dhotii002@gmail.com>
2024-04-01 12:09:16 +0200

    MedicalHistory Updated

--- Domain/Entities/MedicalHistory.cs
+++ Domain/Entities/MedicalHistory.cs
@@ -9,11 +9,11 @@ namespace Domain.Entities
     public class MedicalHistory
     {
         public Guid Id { get; set; }
-        public string Type { get; set; }
-        public string Category { get; set; }
-        public string ICD10 { get; set; }
-        public string Remarks { get; set;}
-        public int PatientId { get; set; }
+        public string AppointmentDate { get; set; }
+        public string Medications { get; set; }
+        public string Surgeries { get; set; }
+        public string RiskFactors { get; set;}
+        public Guid PatientId { get; set; }
         public Patient Patient { get; set; }
 
     }
