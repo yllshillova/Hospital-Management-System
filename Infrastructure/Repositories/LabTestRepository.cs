bd8b926cd6cbbff657b3ef8248649cfa69b7aaeb
vesarrahmani <vesarrahmani17@gmail.com>
2024-04-02 00:02:37 +0100

    Implement LabTest Repository class & interface

---
+++ Infrastructure/Repositories/LabTestRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using Infrastructure.Base;
+
+namespace Infrastructure.Repositories
+{
+    internal sealed class LabTestRepository : EntityBaseRepository<LabTest>, ILabTestRepository
+    {
+        public LabTestRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
