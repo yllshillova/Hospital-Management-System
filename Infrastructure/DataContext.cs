75cdc798580cc9b840963e02a7eebee1068d7872
Yll Shillova <yllshillova@gmail.com>
2024-04-01 12:25:00 +0200

    added doctor entity to dbcontext

--- Infrastructure/DataContext.cs
+++ Infrastructure/DataContext.cs
@@ -14,6 +14,7 @@ namespace Infrastructure
         public DbSet<Department> Departments { get; set; }
         public DbSet<Patient> Patients { get; set; }
         public DbSet<LaboratoryScientist>LaboratoryScientists { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
