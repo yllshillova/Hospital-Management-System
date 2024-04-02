143a2265641ee5de0cd4cccbe2a7dcac3909c570
FazliThaqi <120459098+FazliThaqi@users.noreply.github.com>
2024-04-03 00:31:54 +0200

    update the length of the words in validation

--- Application/Validators/EmergencyContactValidator.cs
+++ Application/Validators/EmergencyContactValidator.cs
@@ -7,7 +7,7 @@ namespace Application.Validators
     {
         public EmergencyContactValidator()
         {
-            RuleFor(d => d.ContactName).NotEmpty().Length(10, 250).WithMessage("ContactName field is required.");
+            RuleFor(d => d.ContactName).NotEmpty().Length(3, 250).WithMessage("ContactName field is required.");
             RuleFor(d => d.Relation).NotEmpty().Length(50).WithMessage("Relation field is required.");
             RuleFor(d => d.PhoneNumber).NotEmpty().WithMessage("PhoneNumber field is required.");
             RuleFor(d => d.PatientID).NotEmpty().WithMessage("PatientID field is required.");
