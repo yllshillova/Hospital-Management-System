360a26e3958eb097e81ce0cdefb9244e14742c2b
Dijane Hoti <dhotii002@gmail.com>
2024-04-02 14:09:53 +0200

    Added sealed to the Patient Repository

--- Infrastructure/Repositories/PatientRepository.cs
+++ Infrastructure/Repositories/PatientRepository.cs
@@ -4,7 +4,7 @@ using Infrastructure.Base;
 
 namespace Infrastructure.Repositories
 {
-    internal class PatientRepository : EntityBaseRepository<Patient>, IPatientRepository
+    internal sealed class PatientRepository : EntityBaseRepository<Patient>, IPatientRepository
     {
         public PatientRepository(DataContext context) : base(context)
         {
