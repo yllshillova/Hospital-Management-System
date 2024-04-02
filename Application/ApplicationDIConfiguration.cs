dfd73496e8c2c47cd1818eaba94dedf7c7e8e331
Yll Shillova <yllshillova@gmail.com>
2024-04-02 15:23:24 +0200

    Added dependency injection pattern in several layers and also added an extension class to improve code readability and organization.

---
+++ Application/ApplicationDIConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Application.Core;
+using Application.DTOs;
+using Application.Validators;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public class ApplicationDIConfiguration
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddScoped<IValidator<DoctorDto>, DoctorValidator>();
+        }
+    }
+}
