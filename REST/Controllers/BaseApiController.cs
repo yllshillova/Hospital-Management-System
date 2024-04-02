940b07b4327f4b56f0f8c45bb8e86066abf5957d
Yll Shillova <yllshillova@gmail.com>
2024-04-03 01:09:10 +0200

    Added result pattern to achieve better organized code and thinner controllers

--- REST/Controllers/BaseApiController.cs
+++ REST/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,7 +13,24 @@ namespace REST.Controllers
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result is null) return NotFound();
+            if (result.IsSuccess && result.Value is not null) return Ok(result.Value);
 
+            return result.ErrorType switch
+            {
+                ErrorType.Unauthorized when !string.IsNullOrEmpty(result.ErrorMessage) => Unauthorized(result.ErrorMessage),
+                ErrorType.Unauthorized => Unauthorized(),
+                ErrorType.NotFound when !string.IsNullOrEmpty(result.ErrorMessage) => NotFound(result.ErrorMessage),
+                ErrorType.NotFound when !result.IsSuccess && result.Value is null => NotFound(),
+                ErrorType.NotFound when result.IsSuccess && result.Value is null => NotFound(),
+                ErrorType.BadRequest when !string.IsNullOrEmpty(result.ErrorMessage) => BadRequest(result.ErrorMessage),
+                ErrorType.BadRequest when !result.IsSuccess => BadRequest(result.ErrorType.ToString()),
+                _ => StatusCode(StatusCodes.Status500InternalServerError)
+            };
+
+        }
 
     }
 }
