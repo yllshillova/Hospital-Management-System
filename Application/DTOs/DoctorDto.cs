6c54925d9c9e51d20eb6d8790b3a73be238e0c06
Yll Shillova <yllshillova@gmail.com>
2024-04-02 14:01:29 +0200

    Added a entitybasedto class, added a doctor dto class and validated it with fluent validation, and added a mappingprofile class to map the dto with the particular entity.

---
+++ Application/DTOs/DoctorDto.cs
@@ -0,0 +1,11 @@
+﻿using Application.Base;
+
+namespace Application.DTOs
+{
+    public class DoctorDto : BaseEntityDto  
+    {
+        public string Specialization { get; set; }
+        public string Qualifications { get; set; }
+        public Guid StaffId { get; set; }
+    }
+}
