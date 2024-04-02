76a605cdc831c6cd468753cd4b7c56526c006ec3
Yll Shillova <yllshillova@gmail.com>
2024-04-02 16:33:58 +0200

    Started implementing the MediatR with CQRS for doctor component and a baseApiController, also started implementing the doctor controller.

---
+++ REST/Controllers/BaseApiController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REST.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+
+
+    }
+}
---
+++ REST/Controllers/DoctorsController.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs;
+using Application.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REST.Controllers
+{
+    public class DoctorsController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IEnumerable<DoctorDto>> GetDoctors()
+        {
+            return await Mediator.Send(new GetDoctorsQuery());
+        }
+    }
+}
