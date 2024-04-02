f7cfff1b63d0a92ae31b44ec5145b8a8c5f5df1e
Dijane Hoti <dhotii002@gmail.com>
2024-04-02 17:40:48 +0200

    Removed two atributes of Patient at the PatientDto and PatientValidator

--- Application/Validators/PatientValidator.cs
+++ Application/Validators/PatientValidator.cs
@@ -11,10 +11,10 @@ namespace Application.Validators
             RuleFor(d => d.Name).NotEmpty().Length(3, 8).WithMessage("Name field is required.");
             RuleFor(d => d.Residence).NotEmpty().Length(10).WithMessage("Residence field is required.");
             RuleFor(d => d.Age).NotEmpty().GreaterThan(0).WithMessage("Age field is required.");
-            RuleFor(d => d.Weight).NotEmpty().GreaterThan(40).WithMessage("Weight field is required.")
+            /*RuleFor(d => d.Weight).NotEmpty().GreaterThan(40).WithMessage("Weight field is required.")
                  .InclusiveBetween(0, 120).WithMessage("Weight must be between 0 and 120.");
             RuleFor(d => d.Height).NotEmpty().GreaterThan(140).WithMessage("Height field is required.")
-                 .GreaterThanOrEqualTo(40).WithMessage("Height must be at least 40.");
+                 .GreaterThanOrEqualTo(40).WithMessage("Height must be at least 40.");*/
             RuleFor(d => d.Gender).NotEmpty().Length(6).WithMessage("Gender field is required.");
             RuleFor(d => d.BloodGroup).NotEmpty().Length(4).WithMessage("BloodGroup field is required.");
 
