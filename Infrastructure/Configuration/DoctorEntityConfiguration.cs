d1b58be49da752843214d89014f8daac0356bd89
Yll Shillova <yllshillova@gmail.com>
2024-04-05 14:52:13 +0200

    Added an configuration extension folder

---
+++ Infrastructure/Configuration/DoctorEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration
+{
+    internal class DoctorEntityConfiguration : IEntityTypeConfiguration<Doctor>
+    {
+        public void Configure(EntityTypeBuilder<Doctor> builder)
+        {
+            builder.HasOne<Staff>(x => x.Staff)
+                   .WithMany()
+                   .HasForeignKey(x => x.StaffId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
