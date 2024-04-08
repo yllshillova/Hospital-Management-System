3630b55f42dc7ed92f02285638e50c0e1220057b
vesarrahmani <vesarrahmani17@gmail.com>
2024-04-08 18:20:58 +0200

    updates

--- Application/EmergencyContacts/EmergencyContactValidator.cs
+++ Application/EmergencyContacts/EmergencyContactValidator.cs
@@ -1,15 +1,21 @@
-﻿using FluentValidation;
+﻿using Application.Doctors;
+using Application.DTOs;
+using Application.EmergencyContacts;
+using FluentValidation;
 
-namespace Application.EmergencyContacts
+namespace Application.BaseValidators
 {
     public class EmergencyContactValidator : AbstractValidator<EmergencyContactDto>
     {
         public EmergencyContactValidator()
         {
-            RuleFor(d => d.ContactName).NotEmpty().Length(3, 30).WithMessage("ContactName field is required.");
-            RuleFor(d => d.Relation).NotEmpty().Length(3, 50).WithMessage("Relation field is required.");
-            RuleFor(d => d.PhoneNumber).NotEmpty().Matches(@"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$").MaximumLength(15).WithMessage("PhoneNumber field is required.");
-            RuleFor(d => d.PatientId).NotEmpty().WithMessage("PatientID field is required.");
+            RuleFor(d => d.ContactName).SetValidator(new NotNullValidator<EmergencyContactDto, string>())
+                                         .SetValidator(new ValidLengthValidator<EmergencyContactDto, string>(4, 100));
+            RuleFor(d => d.Relation).SetValidator(new NotNullValidator<EmergencyContactDto, string>())
+                                         .SetValidator(new ValidLengthValidator<EmergencyContactDto, string>(4, 100));
+            RuleFor(d => d.PhoneNumber).SetValidator(new NotNullValidator<EmergencyContactDto, string>())
+                                         .SetValidator(new ValidLengthValidator<EmergencyContactDto, string>(4, 100));
+            RuleFor(d => d.PatientId).SetValidator(new NotNullValidator<EmergencyContactDto, Guid>());
         }
     }
 }
