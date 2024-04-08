deecaf970fb83e899e1757bc5404aa6b2a0d1c86
vesarrahmani <vesarrahmani17@gmail.com>
2024-04-08 18:16:39 +0200

    updated Room Validator

--- Application/Rooms/RoomValidator.cs
+++ Application/Rooms/RoomValidator.cs
@@ -1,18 +1,16 @@
-﻿using FluentValidation;
+﻿using Application.Rooms;
+using FluentValidation;
 
-<<<<<<<< HEAD:Application/Rooms/RoomValidator.cs
-namespace Application.Rooms
-========
 namespace Application.BaseValidators
->>>>>>>> 19aef6da2b1e1afa6d427be3d384cd4239cf6c30:Application/BaseValidators/RoomValidator.cs
 {
     public class RoomValidator : AbstractValidator<RoomDto>
     {
         public RoomValidator()
         {
-            RuleFor(d => d.Type).NotEmpty().Length(3, 10).WithMessage("Type field is required.");
-            RuleFor(d => d.IsFree).NotNull().WithMessage("IsFree field is required.");
-            RuleFor(d => d.PatientId).NotEmpty().WithMessage("PatientId field is required.");
+            RuleFor(d => d.Type).SetValidator(new NotNullValidator<RoomDto, string>())
+                                            .SetValidator(new ValidLengthValidator<RoomDto, string>(4, 100));
+            RuleFor(d => d.IsFree).SetValidator(new NotNullValidator<RoomDto, bool>());
+            RuleFor(d => d.PatientId).SetValidator(new NotNullValidator<RoomDto, Guid>());
         }
     }
 }
