2b2ad4803425ce0809dcb23825765b613c22db31
vesarrahmani <vesarrahmani17@gmail.com>
2024-04-08 18:19:47 +0200

    updates

--- Application/Nurses/NurseValidator.cs
+++ Application/Nurses/NurseValidator.cs
@@ -1,24 +1,23 @@
-﻿using FluentValidation;
+﻿using Application.Nurses;
+using FluentValidation;
 
-namespace Application.Nurses
+namespace Application.BaseValidators
 {
     public class NurseValidator : AbstractValidator<NurseDto>
     {
         public NurseValidator()
         {
-
-            RuleFor(d => d.Name).NotEmpty().Length(2, 10).WithMessage("Name field is required.");
-            RuleFor(d => d.LastName).NotEmpty().Length(2, 10).WithMessage("Name field is required.");
-            RuleFor(d => d.Residence).NotEmpty().Length(2, 10).WithMessage("Residence field is required.");
-            RuleFor(d => d.Address).NotEmpty().Length(2, 10).WithMessage("Address field is required.");
-            RuleFor(d => d.Gender).NotEmpty().Length(1, 8).WithMessage("Gender field is required.");
-            //isDeleted - ska nevoj 
-            RuleFor(d => d.Birthday).NotNull().Must(BeAValidDate).WithMessage("Birthday must be a valid date.");
-            RuleFor(d => d.DepartmentId).NotEmpty().WithMessage("DepartmentId field is required.");
-        }
-        private bool BeAValidDate(DateTime? date)
-        {
-            return date.HasValue && date.Value.Date <= DateTime.UtcNow.Date;
+            RuleFor(d => d.Name).SetValidator(new NotNullValidator<NurseDto, string>())
+            .SetValidator(new ValidLengthValidator<NurseDto, string>(4, 100));
+            RuleFor(d => d.LastName).SetValidator(new NotNullValidator<NurseDto, string>())
+            .SetValidator(new ValidLengthValidator<NurseDto, string>(4, 100));
+            RuleFor(d => d.Residence).SetValidator(new NotNullValidator<NurseDto, string>())
+            .SetValidator(new ValidLengthValidator<NurseDto, string>(4, 100));
+            RuleFor(d => d.Address).SetValidator(new NotNullValidator<NurseDto, string>())
+            .SetValidator(new ValidLengthValidator<NurseDto, string>(4, 100));
+            RuleFor(d => d.Gender).SetValidator(new NotNullValidator<NurseDto, string>())
+            .SetValidator(new ValidLengthValidator<NurseDto, string>(4, 100));
+            RuleFor(d => d.DepartmentId).SetValidator(new NotNullValidator<NurseDto, Guid>());
         }
     }
 }
