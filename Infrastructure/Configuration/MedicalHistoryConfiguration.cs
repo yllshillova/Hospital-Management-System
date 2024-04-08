f3426803c638dcdae13874a0d53846a4523315cd
Dijane Hoti <dhotii002@gmail.com>
2024-04-08 11:35:50 +0200

    merge

---
+++ Infrastructure/Configuration/MedicalHistoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration
+{
+    internal class MedicalHistoryEntityConfiguration : IEntityTypeConfiguration<MedicalHistory>
+    {
+        public void Configure(EntityTypeBuilder<MedicalHistory> builder)
+        {
+            builder.HasOne(m => m.Patient) 
+                  .WithMany() 
+                  .HasForeignKey(m => m.PatientId)
+                  .OnDelete(DeleteBehavior.SetNull);  //OnDelete(DeleteBehavior.SetNull) do të thotë që kur një pacient fshihet, çelësi i pacientit në tabelën Histori Mjekësore (MedicalHistory)
+                                                      //do të ndryshohet në NULL, por të dhënat në vet Historinë Mjekësore (MedicalHistory) nuk do të fshihen.
+                                                      //Kjo është një mënyrë për të ruajtur të dhënat mjekësore edhe pasi pacienti është fshirë nga sistemi.
+        }
+    }
+}
---
+++ Infrastructure/Configuration/RoomEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration
+{
+    internal class RoomEntityConfiguration : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.HasOne<Patient>(p => p.Patient)
+                   .WithMany()
+                   .HasForeignKey(p => p.PatientId)
+                   .OnDelete(DeleteBehavior.Cascade);  
+        }
+    }
+}
