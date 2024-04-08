eb30c64810350f0b06d320d94ae23d53dcb12487
vesarrahmani <vesarrahmani17@gmail.com>
2024-04-08 16:53:10 +0200

    commented a validation until next time

--- Application/Patients/PatientValidator.cs
+++ Application/Patients/PatientValidator.cs
@@ -16,9 +16,9 @@ namespace Application.Patients
             RuleFor(d => d.Birthday).NotNull().WithMessage("Birthday field is required.").Must(BeAValidDate).WithMessage("Birthday field is not valid.");
             RuleFor(d => d.BloodGroup).NotEmpty().Length(1, 20).WithMessage("BloodGroup field is required.");
             RuleFor(d => d.Gender).NotEmpty().WithMessage("Gender field is required.").Must(HaveValidLength).WithMessage(d => UsernameLengthErrorMessage(d.Gender));
-            RuleFor(x => x.Email).NotEmpty().WithMessage("The email of the employer is required!").EmailAddress()
-            .WithMessage("Invalid email address format.The email should contain \"@\" ").Must(ContainNumberAndDomain)
-            .WithMessage("The email should contain a digit and end either with \".com\" or \".net\"");
+            //RuleFor(x => x.Email).NotEmpty().WithMessage("The email of the employer is required!").EmailAddress()
+            //.WithMessage("Invalid email address format.The email should contain \"@\" ").Must(ContainNumberAndDomain)
+            //.WithMessage("The email should contain a digit and end either with \".com\" or \".net\"");
             RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("The phone number of the employer is required.")
             .Must(BeValidPhoneNumber).WithMessage("Invalid phone number. It should contain exactly 9 digits.");
             RuleFor(d => d.Occupation).NotEmpty().WithMessage("Occupation field is required.").Length(10, 300).WithMessage("Occupation field must be longer.");
