61ea5cc378bfd3d04ec81a86d89af153fab2f98b
Rinnor1 <rinoris100@gmail.com>
2024-04-09 18:40:59 +0200

    Implemented the JWT token for authentication and authorization, along with the Bearer

--- Infrastructure/InfrastructureDIConfiguration.cs
+++ Infrastructure/InfrastructureDIConfiguration.cs
@@ -21,16 +21,17 @@ namespace Infrastructure
                 opt.Password.RequireNonAlphanumeric = false;
                 opt.User.RequireUniqueEmail = true;
             }).AddEntityFrameworkStores<DataContext>();
-            
+
             services.AddScoped<IDoctorRepository, DoctorRepository>();
             services.AddScoped<IVisitRepository, VisitRepository>();
             services.AddScoped<IPatientRepository, PatientRepository>();
             services.AddScoped<IAppointmentRepository, AppointmentRepository>();
-            services.AddScoped<INurseRepository,NurseRepository>();
-            services.AddScoped<IRoomRepository,RoomRepository>();
-            services.AddScoped<IEmergencyContactRepository,EmergencyContactRepository>();
-            services.AddScoped<IDepartmentRepository,DepartmentRepository>();
-            
+            services.AddScoped<INurseRepository, NurseRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+            services.AddScoped<IEmergencyContactRepository, EmergencyContactRepository>();
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<ITokenRepository, TokenRepository>();
+
         }
     }
 }
