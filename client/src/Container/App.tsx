3a277c6edd6035c510e63a3ea4effebd82100e42
Rinnor1 <rinoris100@gmail.com>
2024-04-13 02:58:05 +0200

    created the Dashboard where everything will take place on

--- client/src/Container/App.tsx
+++ client/src/Container/App.tsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { Dashboard } from "../Pages";
+
 function App() {
 
     return (
-    <p>test</p>
-  );
+        <div>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Dashboard />}></Route>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
